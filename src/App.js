1e507ba271ab5fd10334f3f8ae8f4eae6407f39b
IgorGarciaCosta <igonildo7@gmail.com>
2021-07-03 01:27:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import './components/PageNavbar';
 import './App.css';
+import PageNavbar from './components/PageNavbar';
+import React, { Component } from 'react'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PageNavbar />
+        
       </header>
+      <h1 className="tituloDoGrid">Modelos 3D</h1>
     </div>
   );
 }
---
+++ src/components/PageNavbar.js
@@ -0,0 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../components/PageNavbar.css'
+import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+class PageNavbar extends React.Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home" className="titulo">Portifólio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto navItems">
+                        <Nav.Link href="#features">Quem sou eu</Nav.Link>
+                        <Nav.Link href="#pricing">Currículo</Nav.Link>
+                        <NavDropdown title="Contato" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Instagram</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Linkedin</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Deviant art</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    
+                </Navbar.Collapse>
+            </Navbar>
+
+        );
+    }
+}
+
+export default PageNavbar;
